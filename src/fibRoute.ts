4cd225684e86f41e8762411df2ab67fd4876aabc
Jay Parmar <jayhparmar.0707@gmail.com>
2026-01-16 19:57:40 -0500

    Add 'number' type to fibN

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -5,7 +5,7 @@ import fibonacci from "./fib";
 export default (req, res) => {
   const { num } = req.params;
 
-  const fibN = fibonacci(parseInt(String(num)));
+  const fibN: number = fibonacci(parseInt(String(num)));
   let result = `fibonacci(${String(num)}) is ${String(fibN)}`;
 
   if (fibN < 0) {
